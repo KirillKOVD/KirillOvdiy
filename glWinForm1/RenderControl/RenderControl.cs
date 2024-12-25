cc4429ea60f9b4d55eb454082c446ad8ebe05f0e
KirillKOVD <kirillovdiy@gmail.com>
2024-12-25 20:21:06 +0200

    Add files via upload

---
+++ glWinForm1/RenderControl/RenderControl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Design;
+using System.Linq;
+
+namespace glWinForm1
+{
+    public partial class RenderControl : OpenGL
+    {
+        public RenderControl()
+        {
+            InitializeComponent();
+        }
+
+        private void OnRender(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
